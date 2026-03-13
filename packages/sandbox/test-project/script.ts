17863bcea543244aa5c1b32ce1f39392184298c5
ScottMorse <scottmorsedev@gmail.com>
2026-03-13 13:43:40 -0600

    Doc tweaks

--- packages/sandbox/test-project/script.ts
+++ packages/sandbox/test-project/script.ts
@@ -1,9 +1,15 @@
-console.log("argv: ", process.argv.join(" "));
-console.log("BW_PROJECT_PATH:", process.env.BW_PROJECT_PATH);
-console.log("BW_WORKSPACE_NAME:", process.env.BW_WORKSPACE_NAME);
-console.log("BW_WORKSPACE_PATH:", process.env.BW_WORKSPACE_PATH);
-console.log(
-  "BW_WORKSPACE_RELATIVE_PATH:",
-  process.env.BW_WORKSPACE_RELATIVE_PATH,
-);
-console.log("BW_SCRIPT_NAME:", process.env.BW_SCRIPT_NAME);
+// Contents of my-script.ts (in project root)
+
+// [
+//   "my-workspace-name",
+//   "/my/project/path/my-workspace"
+// ]
+console.log(process.argv.slice(2));
+
+// All metadata is available as environment variables for use within a script
+console.log(process.env.BW_PROJECT_PATH);
+console.log(process.env.BW_PROJECT_NAME);
+console.log(process.env.BW_WORKSPACE_NAME);
+console.log(process.env.BW_WORKSPACE_PATH);
+console.log(process.env.BW_WORKSPACE_RELATIVE_PATH);
+console.log(process.env.BW_SCRIPT_NAME);

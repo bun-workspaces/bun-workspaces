9e68518853c6d1f01bebcbbfb510782c97ab6b46
ScottMorse <scottmorsedev@gmail.com>
2026-03-30 15:27:32 -0600

    Setup script arg tests

---
+++ packages/bun-workspaces/tests/features/runScript/scriptArgs.test.ts
@@ -0,0 +1,4 @@
+import { describe, test } from "bun:test";
+import { createFileSystemProject } from "../../../src";
+import { getProjectRoot } from "../../fixtures/testProjects";
+import { setupCliTest } from "../../util/cliTestUtils";

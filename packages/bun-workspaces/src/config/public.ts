1202a581453114b115a1596217a42487fd566f75
ScottMorse <scottmorsedev@gmail.com>
2026-04-09 17:24:55 -0600

    Update type exports

--- packages/bun-workspaces/src/config/public.ts
+++ packages/bun-workspaces/src/config/public.ts
@@ -2,6 +2,9 @@ export type { RootConfig, ResolvedRootConfig } from "./rootConfig";
 export type {
   WorkspaceConfig,
   ResolvedWorkspaceConfig,
+  WorkspaceDependenciesRule,
+  WorkspaceRules,
+  ScriptConfig,
 } from "./workspaceConfig";
 export { defineRootConfig } from "./rootConfig/defineRootConfig";
 export { defineWorkspaceConfig } from "./workspaceConfig/defineWorkspaceConfig";

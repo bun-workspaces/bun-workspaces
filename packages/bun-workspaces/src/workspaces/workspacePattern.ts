ac5eb29ef8cfcba7935154fc63f284735b43f096
ScottMorse <scottmorsedev@gmail.com>
2026-04-09 17:25:59 -0600

    Minor code style

--- packages/bun-workspaces/src/workspaces/workspacePattern.ts
+++ packages/bun-workspaces/src/workspaces/workspacePattern.ts
@@ -33,7 +33,7 @@ export const parseWorkspacePattern = (pattern: string): WorkspacePattern => {
     return {
       target: "default",
       value: patternValue,
-      isNegated: isNegated,
+      isNegated,
     };
   }
 

1ebad9f66ffd81a3453db8696be6c3cc6080fd4f
ScottMorse <scottmorsedev@gmail.com>
2026-04-20 16:41:44 -0600

    Use new patterns in repo

--- workspaces/web/documentation-website/bw.workspace.ts
+++ workspaces/web/documentation-website/bw.workspace.ts
@@ -2,10 +2,5 @@ import { defineWorkspaceConfig } from "bun-workspaces/config";
 
 export default defineWorkspaceConfig({
   alias: ["doc-website", "docs", "docs-website", "docs-web", "documentation"],
-  tags: ["deployable", "static-website", "web"],
-  rules: {
-    workspaceDependencies: {
-      allowPatterns: ["bw-common"],
-    },
-  },
+  tags: ["static-website"],
 });

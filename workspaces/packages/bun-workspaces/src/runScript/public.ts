89dcc583b4fe05cf3947565a4f3d49483ec08c15
ScottMorse <scottmorsedev@gmail.com>
2026-04-27 13:27:27 -0600

    Deprecate type

--- workspaces/packages/bun-workspaces/src/runScript/public.ts
+++ workspaces/packages/bun-workspaces/src/runScript/public.ts
@@ -1,2 +1,7 @@
 export type { WorkspaceScriptMetadataKey } from "bw-common/runScript";
 export { getWorkspaceScriptMetadata } from "./workspaceScriptMetadata";
+
+import type { WorkspaceScriptMetadataKey } from "bw-common/runScript";
+
+/** @deprecated Renamed: Use {@link WorkspaceScriptMetadataKey} instead */
+export type ScriptRuntimeMetadataKey = WorkspaceScriptMetadataKey;

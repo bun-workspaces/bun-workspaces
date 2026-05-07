9f1f439316b702a6b8367699a4683be28b789dab
ScottMorse <scottmorsedev@gmail.com>
2026-05-07 17:41:48 -0600

    Fix test type

--- workspaces/packages/bun-workspaces/src/affected/index.ts
+++ workspaces/packages/bun-workspaces/src/affected/index.ts
@@ -2,3 +2,4 @@ export * from "./fileAffectedWorkspaces";
 export * from "./gitAffectedFiles";
 export * from "./gitAffectedWorkspaces";
 export * from "./affectedBaseRef";
+export * from "./externalDependencyChanges";
